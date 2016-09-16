c05d06f97411bf15880e323cb09b26413777391c
Marcin Krolik <marcin.krolik@intel.com>
2016-09-16 16:46:21 +0200

    Large tests and runnable example

---
+++ examples/tasks/run-mock-load.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+# get the directory the script exists in
+__dir=$(cd $(dirname ${BASH_SOURCE[0]})/../../scripts/docker/large && pwd)
+__proj_dir="$(cd $(dirname ${BASH_SOURCE[0]})/../../ && pwd)"
+__proj_name="$(basename $__proj_dir)"
+
+export PLUGIN_SRC="${__proj_dir}"
+
+# verifies dependencies
+. "${__proj_dir}/examples/tasks/.setup.sh"
+
+# downloads plugins, starts snap, load plugins and start a task
+__id=$(docker run -e SNAP_VERSION=latest -d -v ${PLUGIN_SRC}:/${__proj_name} -v /proc:/var/procfs --net=host mkrolik/snap-pytest:lscpu)
+# clean up containers on exit
+function finish {
+  (docker kill ${__id})
+}
+trap finish EXIT INT TERM
+
+# downloads plugins, starts snap, load plugins and start a task
+cd "${__proj_dir}/examples/tasks" && docker exec -it ${__id} bash -c "PLUGIN_PATH=/etc/snap/plugins /${__proj_name}/examples/tasks/mock-load.sh && printf \"\n\nhint: type 'snapctl task list'\ntype 'exit' when your done\n\n\" && bash"
